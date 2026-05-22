c5aeeed85f52cf25b1ebd6fcfafb8bdf7ef9e9d1
Eigeen <dengyk2002@qq.com>
2026-05-22 10:56:21 +0800

    feat: remove slint version

--- crates/slint_ui/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    std::env::set_var("SLINT_ENABLE_EXPERIMENTAL_FEATURES", "1");
-    slint_build::compile("ui/appwindow.slint").expect("compile Slint UI");
-}
--- crates/slint_ui/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod app;
-mod migration;
-mod path_import;
-
-slint::include_modules!();
-
-fn main() -> eyre::Result<()> {
-    app::run()
-}
